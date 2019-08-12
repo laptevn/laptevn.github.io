c81989399dc9904a5be65167bd8d3466278f54c5
Nickolay Laptev <nickolay.laptev@gmail.com>
2019-08-12 21:53:39 +0200

    Added support of file parameter to form

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
     $("#submit-form").on('submit', function (event) {
         event.preventDefault();
-        var dataString = $(this).serialize();
+        
+        var form = $('#submit-form')[0];
+        var data = new FormData(form);
+
         $('#submit-button').prop('disabled', true);
 
         $.ajax({
             type: "POST",
             url: "https://api.laptev.co/v1",
-            data: dataString,
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
             success: function (data) {
                 $('#submit-form')[0].reset();
             }
